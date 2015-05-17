2084c4db666665c795565292cf9d4d1bb9bdaa95
electrikjesus <electrikjesus@gmail.com>
2015-05-17 14:10:43 -0400

    Added initial Bliss config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,3 +146,19 @@ EXTENDED_FONT_FOOTPRINT := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 MALLOC_IMPL := dlmalloc
+
+# BlissPop Config Flags
+BLISS_WIPE_CACHES := 0
+TARGET_TC_ROM := 4.8-linaro
+TARGET_TC_KERNEL := 4.8-linaro
+BLISSIFY := true
+#BLISS_O3 := true
+#BLISS_STRICT := true
+#BLISS_GRAPHITE := false
+#BLISS_KRAIT := true
+#BLISS_PIPE := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
