80eb74c6b4ae8fd54b4ed77c5a69426ffe11445f
electrikjesus <electrikjesus@gmail.com>
2015-05-17 14:07:14 -0400

    Blissified

--- bliss.mk
+++ bliss.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
@@ -35,6 +35,6 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_flounder
+PRODUCT_NAME := bliss_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
